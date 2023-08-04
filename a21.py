1bb9400e7c18d0efe40527b1d264615f8c29776a
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-04 12:56:16 +0530

    Create a21.py

---
+++ a21.py
@@ -0,0 +1,11 @@
+'''Fibonacci Series'''
+def fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(fibo(n-1) + fibo(n-2))
+
+nterms = int(input("Enter the number of terms : ))
+print("Fibonacci sequence:")
+for i in range(nterms):
+    print(fibo(i))
