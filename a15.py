3f0bc54b50e0472da6b8d0da907a7984b344b4f6
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-04 12:12:57 +0530

    Create a15.py

---
+++ a15.py
@@ -0,0 +1,8 @@
+'''WAP to compute th square root n factorial of a given number'''
+
+import math as noob
+n=int(input())
+result = noob.sqrt(n)
+result1 = noob.factorial(n)
+print("Result is : ",result)
+print("Result1 is : ",result1)
