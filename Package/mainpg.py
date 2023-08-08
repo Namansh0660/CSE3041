8e1e8424890a6f00f3ab4eb7c0394cf87c1c0d5b
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-08 15:41:54 +0530

    Update mainpg.py

--- Package/mainpg.py
+++ Package/mainpg.py
@@ -3,6 +3,34 @@
 # from <package_name> import <module>
 from Package import mod1
 from Package import mod2 
+
+#Methods of importing a package/module
+
+'''
+from My_package import mod1
+from My_package import mod2
+'''
+
+'''
+import My_package.mod1
+import My_package.mod2
+'''
+
+'''
+import mod1
+import mod2
+'''
+
+'''
+import My_package.mod1 as m1
+import My_package.mod2 as m2
+'''
+
+mod1.abc()
+x = int(input("Enter the first number :"))
+y = int(input("Enter the second number : "))
+mod2.add(x,y)
+
 mod1.abc()
 x = int(input("Enter first number :"))
 y = int(input("Enter second number :"))
