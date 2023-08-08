efd4832048e0426157bec24d2d2b803398bbeba6
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-08 15:29:22 +0530

    Main File we need to run this

--- Package/mainpg.py
+++ Package/mainpg.py
@@ -2,7 +2,7 @@
 
 # from <package_name> import <module>
 from Package import mod1
-from Package import mod2
+from Package import mod2 
 mod1.abc()
 x = int(input("Enter first number :"))
 y = int(input("Enter second number :"))
