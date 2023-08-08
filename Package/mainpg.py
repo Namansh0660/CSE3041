5a4ee58c757ec2dc2793f5449632669a58b94651
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-08 15:28:58 +0530

    Create mainpg.py
    Main File we need to run this

---
+++ Package/mainpg.py
@@ -0,0 +1,9 @@
+'''Main File we need to run this'''
+
+# from <package_name> import <module>
+from Package import mod1
+from Package import mod2
+mod1.abc()
+x = int(input("Enter first number :"))
+y = int(input("Enter second number :"))
+mod2.sum(x,y)
