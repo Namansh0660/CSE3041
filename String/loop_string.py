d4117a875bd16f89e2cedf824c88085d222b5023
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-11 13:03:27 +0530

    Create loop_string.py

---
+++ String/loop_string.py
@@ -0,0 +1,7 @@
+for x in "Python":
+  print(x,end=" ")                 # P y t h o n
+
+
+str = "Welcome to Data Science"
+print("to" in str)                 # true
+print("Python" in str)             # false 
