beeb52bbe490f176653864e12da807b0432dc42e
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-11 12:51:30 +0530

    Create escape_sequence.py

---
+++ String/escape_sequence.py
@@ -0,0 +1,10 @@
+str = '''I'm a "student"'''
+print(str)                          # I'm a "student"
+str='I\'m a "student"'
+print(str)                          # I'm a "student"
+str = "I'm a \"student\""
+print(str)                          # I'm a "student"
+str1 = "Data\tScience"
+print(str1)                         # Data Science
+str2 = "Data\nScience"
+print(str2)                         # Data \n Science
