74fa6d67e8082d8b40021b2c97bb706c4edc7d99
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-11 12:41:58 +0530

    Create string3.py

---
+++ String/string3.py
@@ -0,0 +1,10 @@
+str='Welcome to Python'
+str1 = str[0:2]+'e'+str[2:]                  # Weelcome to Python
+print(str1)
+
+
+str='Welcome to Python'                      # Both will be printed
+print(str)
+str='Introduction to Data Science'
+print(str)
+
