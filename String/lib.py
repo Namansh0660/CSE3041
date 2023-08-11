6dcc58b7e01db952d7635a367d8d4fd010ed7028
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-11 13:12:49 +0530

    Create lib.py

---
+++ String/lib.py
@@ -0,0 +1,16 @@
+str = 'python for Data Science'
+print(len(str))                       # 23
+print(str.upper())                    # PYTHON FOR DATA SCIENCE
+print(str.lower())                    # python for data science
+print(str.replace('for',4))           # python 4 Data Science
+print(str.strip())                    # python for Data Science
+print(str.capitalize())               # Python for Data Science
+print(str.islower())                  # false
+print(str.find('for'))                # 7
+print(str.count('a'))                 # 2
+print(str.isspace())                  # false
+print(str.isdigit())                  # false
+str1 = "Python"
+str2 = "DataScience"
+print(str1==str2)                     # false
+print(str1+str2)                      # PythonDataScience
