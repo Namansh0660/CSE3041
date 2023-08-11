484aab26334633a5149bc3f3f467bf2393b852a0
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-11 12:38:25 +0530

    Create string2.py

---
+++ String/string2.py
@@ -0,0 +1,7 @@
+str = 'welcome to python'
+print(str[3:7])                #come
+print(str[:6])                 #welcom
+print(str[4:])                 #ome to python
+print(str[7:-2])               #to pyth
+str = 'Welcome to python'
+print(".join(reversed(str)")   #.join(reversed(str)
