4f954223ca7d2ed35b7ed85a2cb666c89d793239
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-17 12:26:07 +0530

    operations in tuple

---
+++ Tuple/operations.py
@@ -0,0 +1,12 @@
+str=(1,2,3,4,5)
+str1=("a","b","c")
+str2=(1,2,2.5,"a","b")
+str3=(50,)
+str4=()
+print(len(str))     # length
+str3=str1+str2
+print(str3)         # concatenation
+print(str1 * 3)     # Repetition
+print(1 in str)     # Membership
+for i in python :
+  print(i)          # Iteration
