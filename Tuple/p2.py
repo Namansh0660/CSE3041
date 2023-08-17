d809bfc6c035aaafaf43e1b63efcc8559cc02a8a
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-17 12:35:56 +0530

    Create p2.py

---
+++ Tuple/p2.py
@@ -0,0 +1,17 @@
+str=(1,2,3,4,5,6,7,8,9)
+print(len(str))
+print(str.count(2))
+print(min(str))
+print(max(str))
+str1=(1,2,3,"a","b",2.4,true)
+print(min(str))
+
+
+
+str1=(1,2,3,4,5)
+str2=(3,4,5,6)
+str=(str1,str1)
+print(str)
+print(len(str))
+print(str1+str2)
+
