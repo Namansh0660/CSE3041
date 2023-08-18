98bf629ce88b615194589e0d500ff2d49790f4e6
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-18 12:29:02 +0530

    Create operations.py

---
+++ Lists/operations.py
@@ -0,0 +1,12 @@
+list=[1,2,3,4,5]
+list1=["a","b","c"]
+list2=[1,2,2.5,"a","b"]
+list3=[50,]
+list4=[]
+print(len(list))     # length
+list3=list1+list2
+print(list3)         # concatenation
+print(list1 * 3)     # Repetition
+print(1 in list)     # Membership
+for i in list2 :
+  print(i,end='\t')          # Iteration
