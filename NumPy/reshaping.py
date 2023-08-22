eef029f91d3970ac8fe78f4fa811a74db8ff3773
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-22 14:45:10 +0530

    Create reshaping.py

---
+++ NumPy/reshaping.py
@@ -0,0 +1,12 @@
+'''  1D to 2D '''
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3)
+print(newarr)
+
+
+'''  1D to 3D '''
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(2, 3, 2)
+print(newarr)
