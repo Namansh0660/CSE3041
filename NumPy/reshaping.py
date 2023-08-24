2c56d3dbe771cef4c0170b750383308ca74d8874
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-24 12:58:56 +0530

    Update reshaping.py

--- NumPy/reshaping.py
+++ NumPy/reshaping.py
@@ -6,7 +6,17 @@ print(newarr)
 
 
 '''  1D to 3D '''
-import numpy as np
 arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 newarr = arr.reshape(2, 3, 2)
+print(newarr)
+
+''' Unknown dimenion '''
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+newarr = arr.reshape(2, 2, -1)
+print(newarr)
+
+
+''' taking (-1) '''
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+newarr = arr.reshape(-1)
 print(newarr)
