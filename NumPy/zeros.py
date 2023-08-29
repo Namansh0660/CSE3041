0d8e697cfa66fa73c4afb2bc113565a2e6d1dcc9
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-29 15:10:31 +0530

    Create zeros.py

---
+++ NumPy/zeros.py
@@ -0,0 +1,9 @@
+import numpy as np
+y=np.zeros(3)
+print('\n',y)
+y=np.zeros(3,dtype='int')
+print('\n',y)
+y=np.zeros([2,2],dtype='int')
+print('\n',y)
+y=np.zeros([3,3],dtype='int')
+print('\n',y)
