7d87b908324fe2290d87e603000e69db9120fc10
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-29 14:58:34 +0530

    Create eigen.py

---
+++ NumPy/eigen.py
@@ -0,0 +1,22 @@
+a = np.array([[1,-2j], [2j,5]])
+print("\nArray is :",a)
+c,d = np.linalg.eigh(a)
+print("Eigen value is :", c)
+print("Eigen value is :", d)
+
+
+a = np.diag((1,2,3))
+print("\nArray is :",a)
+c, d = np.linalg.eig(a)
+print("Eigen value is :", c)
+print("Eigen value is :",d)
+
+
+'''linalg.eigvals(a)'''
+a = np.diag((1, 2, 3))
+print('\n',np.linalg.eigvals(a))
+
+
+'''eigvalsh'''
+a = np.array([[1, -2j], [2j, 5]])
+print('\n',np.linalg.eigvalsh(a))
