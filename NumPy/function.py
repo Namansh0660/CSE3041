ec98a318f9779a136cd606ff0693e8ae07646659
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-08-29 14:34:33 +0530

    Create function.py

---
+++ NumPy/function.py
@@ -0,0 +1,13 @@
+''' Inner function '''
+a = np.array([[1,3],[5,7]])
+b = np.array([[2,4],[6,8]])
+result = np.inner(a,b)
+print(result)
+print("\n")
+
+''' Outer Function '''
+a = np.array([[1,3],[5,7]])
+b = np.array([[2,4],[6,8]])
+result = np.outer(a,b)
+print(result)
+print("\n")
