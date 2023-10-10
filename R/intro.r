46f64cbef7173666270d14ba153741939b85aca1
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-10-10 14:52:50 +0530

    Create intro.r

---
+++ R/intro.r
@@ -0,0 +1,143 @@
+5+5
+
+"Hello World"
+print("Hello World")
+
+"Welcome to R" # This is the first program
+
+# This is an example of
+# multi line comment
+# in r Programming
+"Welcome to R"
+
+name <- "John"
+age <- 40
+name
+
+name <- name1 <- name2 <- 'John'
+
+a <- 30
+a <- 'John'
+a
+
+x <- 10.5
+class(x)
+
+x <- 1000L
+class(x)
+
+x <- 9i+3
+class(x)
+
+x <- "R is exciting"
+class(x)
+
+x <- TRUE
+class(x)
+
+x <- 10.5
+y <- 55
+x
+y
+class(x)
+class(y)
+
+x <- 1000L
+y <- 55L
+x
+y
+class(x)
+class(y)
+
+x <- 3+5i
+y <- 5i
+x
+y
+class(x)
+class(y)
+
+x <- 1L #integer
+y <- 2 #numeric
+a <- as.numeric(x)
+b <- as.integer(y)
+a
+class(a)
+b
+class(b)
+
+max(5,10,15)
+min(5,10,15)
+sqrt(16)
+abs(-4.7)
+ceiling(1.4)
+floor(1.4)
+
+str <- "Hello"
+str
+str1 <- "This is my first
+R program executed in R language
+and R is so interactive and
+interesrting to work"
+str1
+
+str <- "Welcome to R programming"
+nchar(str)
+
+grepl("W",str)
+grepl("R",str)
+grepl("o",str)
+grepl("m",str)
+
+str1<- "Welcome to"
+str2 <- "R programming"
+paste(str1,str2)
+
+str <- "Welcome to \"the most interactive\",R proramming"
+str
+cat(str)
+
+4 > 5
+6 > 7
+5 == 9
+
+a <- 10
+b <- 5
+a > b
+a + b
+a - b
+a * b
+a / b
+a %% b
+a ^ b
+a %/% b
+
+str <- 3
+str
+str1 <<- 5
+str1
+2 -> str2
+str2
+4 ->> str3
+str3
+
+a <- 10
+b <- 5
+a > b
+a > b
+a == b
+a != b
+a >= b
+a <= b
+
+a & b 
+a | b
+(a>b) && (a==b)
+(a>b) || (a==b)
+!a
+
+x <- 1:10
+x
+
+a %in% b
+
+x <- Matrix1 %*% Matrix2
