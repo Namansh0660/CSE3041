aef9bb10f85d4430a50ab6045f19880d0c4cec6c
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-10-24 14:38:24 +0530

    Create Input_Output.r

---
+++ R/Input_Output.r
@@ -0,0 +1,19 @@
+var = readline("Enter a number : ");
+var = as.integer(var);
+print(var)
+
+var1 = readline(" Enter a number1");
+var2 = readline(" Enter a number2");
+var3 = readline(" Enter a number3");
+var1 = as.integer(var1);
+var2 = as.integer(var2);
+var3 = as.integer(var3);
+print(var1+var2+var3)
+
+
+var1 = readline(" Enter name");
+var2 = readline(" Enter initial");
+var1 = as.character(var1);
+var2 = as.character(var2);
+print(var1)
+print(var2)
