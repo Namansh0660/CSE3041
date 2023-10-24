599eef4f0f369947de025081d06095a2c8e18d81
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-10-24 15:29:13 +0530

    Update Input_Output.r

--- R/Input_Output.r
+++ R/Input_Output.r
@@ -2,18 +2,38 @@ var = readline("Enter a number : ");
 var = as.integer(var);
 print(var)
 
-var1 = readline(" Enter a number1");
-var2 = readline(" Enter a number2");
-var3 = readline(" Enter a number3");
+var1 = readline(" Enter a number1 : ");
+var2 = readline(" Enter a number2 : ");
+var3 = readline(" Enter a number3 : ");
 var1 = as.integer(var1);
 var2 = as.integer(var2);
 var3 = as.integer(var3);
 print(var1+var2+var3)
 
 
-var1 = readline(" Enter name");
-var2 = readline(" Enter initial");
+var1 = readline(" Enter name : ");
+var2 = readline(" Enter initial : ");
 var1 = as.character(var1);
 var2 = as.character(var2);
 print(var1)
 print(var2)
+
+
+x = scan()
+print(x)
+
+
+do = scan(what = double())
+str = scan(what = "")
+ch = scan(what = character())
+print(do) 
+print(str) 
+print(ch) 
+
+
+var1="Programming for Data Science"
+print(var1)
+var2="Welcome to R programming\n"
+cat("Hello", var2)
+var3="Python\n"
+message("Hi",var3)
